b27e989733137b7f2e6119eb47098f0ccb62b4be
Hamze Jihad <hamze@pactosolucoes.com.br>
2021-03-07 20:39:12 -0300

    UI login feita

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:projetoestrutura/home.dart';
 
+final ThemeData padrao = ThemeData(
+  primaryColor: Color(0xff9c27b0),
+  accentColor: Color(0xff7b1fa2),
+);
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
+    theme: padrao ,
   ));
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
 
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
 
